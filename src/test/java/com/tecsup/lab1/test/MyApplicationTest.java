292d849942f20cd44ff5f22496d6f26bdf36d7e1
Jaibey Pisco <jaibey.pisco@tecsup.edu.pe>
2021-08-23 11:47:30 -0500

    Subiendo el proyecto del lab 01

---
+++ src/test/java/com/tecsup/lab1/test/MyApplicationTest.java
@@ -0,0 +1,17 @@
+package com.tecsup.lab1.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.tecsup.lab1.MyApplication;
+
+public class MyApplicationTest {
+
+
+	@Test
+	public void testApp() {
+		MyApplication appObject = new MyApplication();
+		Assert.assertEquals(appObject.reverseString("Hola!"), "!aloH");
+	}
+
+}
